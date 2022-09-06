40e0f04f60e6cae05c6a3525d1d8eca2013dd7a8
KolyaSotnichenko <kolya.sotnichenko0811@gmail.com>
2022-09-06 14:51:03 +0300

    redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,12 +1,29 @@
 const redis = require('redis')
 const publisher = redis.createClient([process.env.REDIS_URL, {legacyMode: true}])
 
+publisher.on('error', function(err) {
+    console.log('*Redis Client Error: ' + err.message)
+})
+
+publisher.on('connect', function(){
+    console.log('Connected to redis instance');
+})
+
+
 exports.publishMessage = (channel, message) => {
     publisher.publish(channel, message)
 }
 
 exports.getSubscriber = (channel) => {
-    const subscriber = redis.createClient(process.env.REDIS_URL)
+    const subscriber = redis.createClient([process.env.REDIS_URL, {legacyMode: true}])
+    subscriber.on('error', function(err) {
+        console.log('*Redis Client Error: ' + err.message)
+    })
+    
+    subscriber.on('connect', function(){
+        console.log('Connected to redis instance');
+    })
+    
     subscriber.subscribe(channel)
     return subscriber
 }

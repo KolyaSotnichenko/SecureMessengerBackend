51cd6db2aa48dbe14f19024bdb4f3d87ac84f2d8
KolyaSotnichenko <kolya.sotnichenko0811@gmail.com>
2022-09-06 18:41:06 +0300

    redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,11 +1,21 @@
 const redis = require('redis')
-const publisher = redis.createClient({
-    url: process.env.REDIS_URL,
-    socket: {
-      tls: true,
-      rejectUnauthorized: false
-    }
-  });
+
+const host = 'awesome.redis.server'
+const port = 6380
+
+const url = `redis://${host}:${port}`
+const publisher = createClient({ url })
+
+// const publisher = redis.createClient({
+//     url: process.env.REDIS_URL,
+//     socket: {
+//       tls: true,
+//       rejectUnauthorized: false
+//     }
+//   });
+
+//   publisher.on('error', (err) => console.log('Redis Client Error', err));
+//   publisher.connect()
 
 exports.publishMessage = (channel, message) => {
     publisher.publish(channel, message)
@@ -14,13 +24,18 @@ exports.publishMessage = (channel, message) => {
 
 
 exports.getSubscriber = (channel) => {
-    const subscriber = redis.createClient({
-        url: process.env.REDIS_URL,
-        socket: {
-          tls: true,
-          rejectUnauthorized: false
-        }
-      });
+    // const subscriber = redis.createClient({
+    //     url: process.env.REDIS_URL,
+    //     socket: {
+    //       tls: true,
+    //       rejectUnauthorized: false
+    //     }
+    //   });
+    const host = 'awesome.redis.server'
+    const port = 6380
+
+    const url = `redis://${host}:${port}`
+    const publisher = createClient({ url })
     subscriber.subscribe(channel)
     return subscriber
 }

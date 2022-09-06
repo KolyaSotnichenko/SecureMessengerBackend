d6af80a4aaa777ba02c8f7bf577e112bab83c830
KolyaSotnichenko <kolya.sotnichenko0811@gmail.com>
2022-09-06 18:17:29 +0300

    redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,5 +1,11 @@
 const redis = require('redis')
-const publisher = redis.createClient(process.env.REDIS_URL)
+const publisher = redis.createClient({
+    url: process.env.REDIS_URL,
+    socket: {
+      tls: true,
+      rejectUnauthorized: false
+    }
+  });
 
 exports.publishMessage = (channel, message) => {
     publisher.publish(channel, message)
@@ -8,7 +14,13 @@ exports.publishMessage = (channel, message) => {
 
 
 exports.getSubscriber = (channel) => {
-    const subscriber = redis.createClient(process.env.REDIS_URL)
+    const subscriber = redis.createClient({
+        url: process.env.REDIS_URL,
+        socket: {
+          tls: true,
+          rejectUnauthorized: false
+        }
+      });
     subscriber.subscribe(channel)
     return subscriber
 }

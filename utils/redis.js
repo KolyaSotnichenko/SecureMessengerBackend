846f2fe69fcf16feba9ada0fd18042176f9e2d26
KolyaSotnichenko <kolya.sotnichenko0811@gmail.com>
2022-09-06 16:17:02 +0300

    redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,13 +1,17 @@
 const redis = require('redis')
 const publisher = redis.createClient(process.env.REDIS_URL)
+const subscriber = redis.createClient(process.env.REDIS_URL)
+
+await subscriber.connect()
 
 exports.publishMessage = (channel, message) => {
     publisher.publish(channel, message)
 }
 
-exports.getSubscriber = async (channel) => {
-    const subscriber = redis.createClient(process.env.REDIS_URL)
-    await subscriber.connect()
+
+
+exports.getSubscriber = (channel) => {
+    
     subscriber.subscribe(channel)
     return subscriber
 }

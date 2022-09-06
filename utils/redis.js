d53012abddb49e7445f3a207851227b8b86cefff
KolyaSotnichenko <kolya.sotnichenko0811@gmail.com>
2022-09-06 17:55:25 +0300

    redis

--- utils/redis.js
+++ utils/redis.js
@@ -7,10 +7,10 @@ exports.publishMessage = (channel, message) => {
 
 
 
-exports.getSubscriber = async (channel) => {
+exports.getSubscriber = (channel) => {
     const subscriber = redis.createClient({url: process.env.REDIS_URL, legacyMode: true})
     subscriber.connect()
-    await subscriber.v4.set('key', 'value', {
+    subscriber.v4.set('key', 'value', {
         NX: true
     });
     subscriber.subscribe(channel)

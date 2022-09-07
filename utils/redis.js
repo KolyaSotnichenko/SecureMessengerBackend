d2c9f120241afe99546b0426ed560eb32d71793a
KolyaSotnichenko <kolya.sotnichenko0811@gmail.com>
2022-09-07 17:42:05 +0300

    redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,8 +1,8 @@
 const redis = require('redis')
 var url = require('url');
 // var redisURL = url.parse(process.env.REDISCLOUD_URL);
-const publisher = redis.createClient(process.env.REDISCLOUD_URL || 'redis://default:cORDVjgxKeLbpOwTNDF2SJQr4J56VvGy@redis-19695.c78.eu-west-1-2.ec2.cloud.redislabs.com:19695', {no_ready_check: true, legacyMode: true});
-const subscriber = redis.createClient(process.env.REDISCLOUD_URL || 'redis://default:cORDVjgxKeLbpOwTNDF2SJQr4J56VvGy@redis-19695.c78.eu-west-1-2.ec2.cloud.redislabs.com:19695', {no_ready_check: true, legacyMode: true});
+const publisher = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true, legacyMode: true});
+const subscriber = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true, legacyMode: true});
 
 (async () => {
     try {

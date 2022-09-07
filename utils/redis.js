6b2658ee3470151457e8c458e282910d65971a24
KolyaSotnichenko <kolya.sotnichenko0811@gmail.com>
2022-09-07 17:07:41 +0300

    redis

--- utils/redis.js
+++ utils/redis.js
@@ -6,15 +6,15 @@ const { endpointUri, password } = require('../config').redis;
 const publisher = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
 const subscriber = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
 
-// (async () => {
-//     try {
-//         await publisher.connect()
-//         await subscriber.connect()
-//     } catch (error) {
-//         console.log('error while connecting redis', error)
-//         console.log(endpointUri)
-//     }
-// })()
+(async () => {
+    try {
+        await publisher.connect()
+        await subscriber.connect()
+    } catch (error) {
+        console.log('error while connecting redis', error)
+        console.log(endpointUri)
+    }
+})()
 
 exports.publishMessage = (channel, message) => {
     publisher.publish(channel, message)

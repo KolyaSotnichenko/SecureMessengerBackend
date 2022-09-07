d175da70fee0eb2f8be440659d82d8446cbec5f1
KolyaSotnichenko <kolya.sotnichenko0811@gmail.com>
2022-09-07 14:17:55 +0300

    new config for redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,24 +1,14 @@
 const redis = require('redis')
-const publisher = redis.createClient({
-    url: process.env.REDIS_URL,
-    socket: {
-      tls: true,
-      rejectUnauthorized: false
-    }
-  });
+
+const { endpointUri, password } = require('../config').redis;
+const publisher = redis.createClient(`redis://${endpointUri}`, {password});
 
 exports.publishMessage = (channel, message) => {
     publisher.publish(channel, message)
 }
 
 exports.getSubscriber = (channel) => {
-    const subscriber = redis.createClient({
-        url: process.env.REDIS_URL,
-        socket: {
-          tls: true,
-          rejectUnauthorized: false
-        }
-      });
+    const subscriber = redis.createClient(`redis://${endpointUri}`, {password});
     subscriber.subscribe(channel)
     return subscriber
 }

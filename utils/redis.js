fc9548d3437f80195ca143a0aefe24d42a091c06
KolyaSotnichenko <kolya.sotnichenko0811@gmail.com>
2022-09-07 16:42:55 +0300

    redis

--- utils/redis.js
+++ utils/redis.js
@@ -3,8 +3,8 @@ var url = require('url');
 
 const { endpointUri, password } = require('../config').redis;
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
-const publisher = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-const subscriber = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+const publisher = redis.createClient(redisURL.port, redisURL.hostname, password, {no_ready_check: true});
+const subscriber = redis.createClient(redisURL.port, redisURL.hostname, password, {no_ready_check: true});
 
 (async () => {
     try {
